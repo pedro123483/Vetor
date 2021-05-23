5c802041aa04f3ae754e284b40298eb4ae5ce9b0
Pedro Silva <pg308758@gmail.com>
2021-05-23 09:34:24 -0300

    vetor

---
+++ ex1.java
@@ -0,0 +1,40 @@
+package matrizevetor;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		
+		int[] arraya = new int[6];
+		int soma;
+		
+		arraya[0] = 1;
+		arraya[1] = 0;
+	    arraya[2] = 5;
+	    arraya[3] = -2;
+	    arraya[4] = 100;
+	    arraya[5] = 7;
+	    
+	    soma = arraya[0] + arraya[1] + arraya[5];
+	    
+	    System.out.println("O valor do vetor zero é: " + arraya[0]);
+	    System.out.println("O valor do vetor um é: " + arraya[1]);
+	    System.out.println("O valor do vetor dois é: " + arraya[2]);
+	    System.out.println("O valor do vetor três é: " + arraya[3]);
+	    System.out.println("O valor do vetor quatro é: " + arraya[4]);
+	    System.out.println("O valor do vetor cinco é: " + arraya[5]);
+	    System.out.println("A soma entre os valores da posição a[0], a[1] e a[5] é: " + soma);
+	    
+	    
+	    
+	    		
+	    		
+		
+		
+		
+		
+				
+	
+
+	}
+
+}
